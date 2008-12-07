0d07a4d9d03f47f91f0f5a23783183b2da396fab
Christian Oudard <christian.oudard@gmail.com>
2008-12-07 12:41:51 -0500

    Minor speed improvement in 004.

--- 004.py
+++ 004.py
@@ -1,23 +1,22 @@
 def is_palindrome(num):
-	s = str(num)
-	return s == str_reverse(s)
+    s = str(num)
+    return s == str_reverse(s)
 
 def str_reverse(s):
-	return ''.join(reversed(s))
+    return ''.join(reversed(s))
 
 def iter_all_3digit_pairs():
-	three_digit_numbers = list(range(100, 1000))
-	for a in three_digit_numbers:
-		for b in three_digit_numbers:
-			yield a, b
+    for a in range(100, 1000):
+        for b in range(a, 1000):
+            yield a, b
 
 l = []
 d = {}
 for a, b in iter_all_3digit_pairs():
-	product = a * b
-	if is_palindrome(product):
-		l.append(product)
-		d[product] = (a, b)
+    product = a * b
+    if is_palindrome(product):
+        l.append(product)
+        d[product] = (a, b)
 m = max(l)
 a, b = d[m]
 assert(a * b == m)
