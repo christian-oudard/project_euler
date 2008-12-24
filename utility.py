759b55ef7bbe45e9c606a1e04782c5c0f6a83709
Christian Oudard <christian.oudard@gmail.com>
2008-12-24 10:56:40 -0500

    Added Euler's totient function.

--- utility.py
+++ utility.py
@@ -126,6 +126,19 @@ def is_probable_prime(n):
 
     return True # no trial number showed n as composite
 
+def totient(n):
+    """
+    Euler's totient function.
+
+    >>> [totient(n) for n in range(1, 70)]
+    [1, 1, 2, 2, 4, 2, 6, 4, 6, 4, 10, 4, 12, 6, 8, 8, 16, 6, 18, 8, 12, 10, 22, 8, 20, 12, 18, 12, 28, 8, 30, 16, 20, 16, 24, 12, 36, 18, 24, 16, 40, 12, 42, 20, 24, 22, 46, 16, 42, 20, 32, 24, 52, 18, 40, 24, 36, 28, 58, 16, 60, 30, 36, 32, 48, 20, 66, 32, 44]
+    """
+
+    totient = n
+    for p in set(prime_factorization(n)):
+        totient -= totient // p
+    return totient
+
 def up_to(n, iterable):
     return list(itertools.takewhile(lambda i: i <= n, iterable))
 
