33a877d685c0947c95d2bb66ab8ab8c5389605a4
Christian Oudard <christian.oudard@gmail.com>
2009-06-29 15:45:57 -0400

    Added problem 27.

--- utility.py
+++ utility.py
@@ -153,6 +153,7 @@ def is_probable_prime(n):
 def totient(n):
     """
     Euler's totient function.
+    Find the number of positive integers less than or equal to n that are coprime to n.
 
     >>> [totient(n) for n in range(1, 70)]
     [1, 1, 2, 2, 4, 2, 6, 4, 6, 4, 10, 4, 12, 6, 8, 8, 16, 6, 18, 8, 12, 10, 22, 8, 20, 12, 18, 12, 28, 8, 30, 16, 20, 16, 24, 12, 36, 18, 24, 16, 40, 12, 42, 20, 24, 22, 46, 16, 42, 20, 32, 24, 52, 18, 40, 24, 36, 28, 58, 16, 60, 30, 36, 32, 48, 20, 66, 32, 44]
