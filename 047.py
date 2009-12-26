5c0bf03b56658ba37b50d636e01f0e2692fe1a66
Christian Oudard <christian.oudard@gmail.com>
2009-12-26 10:14:18 -0500

    Added a comment on a better algorithm for number 47.

--- 047.py
+++ 047.py
@@ -11,3 +11,9 @@ for n, counts in enumerate(tuplewise(size, factor_counts())):
     if all(c >= size for c in counts):
         print(n)
         break
+
+# This can be done faster by going the other way, generating the number of
+# prime factors for every number under a limit by enumerating primes and adding
+# to a table of prime factor counts.
+# Equivalently, a memoized prime_factorization function would run with the same
+# complexity.
