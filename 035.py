04f78fb284eb19acdcfef599a37e18d2127de820
Christian Oudard <christian.oudard@gmail.com>
2009-12-28 11:59:53 -0500

    Added problem 35.

---
+++ 035.py
@@ -0,0 +1,30 @@
+from utility import primes, up_to
+
+def rot_left(s):
+    """
+    >>> rot_left('a')
+    'a'
+    >>> rot_left('abc')
+    'bca'
+    """
+    return s[1:] + s[0]
+
+def rotations(n):
+    """
+    >>> list(rotations(1))
+    [1]
+    >>> list(rotations(123))
+    [123, 231, 312]
+    """
+    s = str(n)
+    for _ in s:
+        yield int(s)
+        s = rot_left(s)
+
+def circular_primes(limit):
+    prime_set = set(up_to(limit, primes()))
+    for p in prime_set:
+        if all((r in prime_set) for r in rotations(p)):
+            yield p
+
+print(len(list(circular_primes(1000000))))
