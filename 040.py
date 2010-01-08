089a611f501b9dc335ace5c082cf5f9c3aaf1131
Christian Oudard <christian.oudard@gmail.com>
2010-01-07 20:00:11 -0500

    Added problem 40.

---
+++ 040.py
@@ -0,0 +1,24 @@
+import itertools
+from utility import digits_of, product
+
+def iter_counting_digits():
+    """
+    >>> from utility import nth
+    >>> nth(iter_counting_digits(), 12 - 1)
+    1
+    >>> ''.join(str(d) for d in itertools.islice(iter_counting_digits(), 33))
+    '123456789101112131415161718192021'
+    """
+    for i in itertools.count(1):
+        for d in digits_of(i):
+            yield d
+
+nums = {10**p - 1 for p in range(7)}
+max_num = max(nums)
+results = []
+for i, d in enumerate(iter_counting_digits()):
+    if i in nums:
+        results.append(d)
+        if len(results) >= len(nums):
+            break
+print(product(results))
