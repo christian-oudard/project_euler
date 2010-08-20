d7fac3375e2ee987e792c9acede45a12dfb47b96
Christian Oudard <christian.oudard@gmail.com>
2010-08-20 12:54:19 -0400

    Working on problem 62, currently incorrect.

--- 062.py
+++ 062.py
@@ -1,13 +1,23 @@
+import itertools
+from collections import defaultdict
 from utility import digits_of, all_equal
 
+#The cube, 41063625 (3453), can be permuted to produce two other cubes: 56623104 (3843) and 66430125 (4053). In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.
+#
+#Find the smallest cube for which exactly five permutations of its digits are cube.
 def cube_permutations(size):
     """
-    >>> cube_permutations(3)
-    {345, 384, 405}
+    >>> sorted(cube_permutations(3))
+    [345, 384, 405]
     """
-    pass
+    cubes_by_digits = defaultdict(list)
+    for i in itertools.count(1):
+        cube = i**3
+        digits = tuple(sorted(digits_of(cube)))
+        cubes = cubes_by_digits[digits]
+        cubes.append(i)
+        if len(cubes) >= size:
+            return set(cubes)
 
 if __name__ == '__main__':
-    # Verify given solution.
-    digits = [sorted(digits_of(n**3)) for n in {345, 384, 405}]
-    assert all_equal(digits)
+    print(min(cube_permutations(5)) ** 3)
